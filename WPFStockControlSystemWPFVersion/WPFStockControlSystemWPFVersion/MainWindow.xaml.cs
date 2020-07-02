1f0f4ebfa188b6542cd489b7b186343d5b795d63
thelolinator2000 <48604562+thelolinator2000@users.noreply.github.com>
2020-07-02 15:30:36 +0100

    Awais Hello World Test

--- WPFStockControlSystemWPFVersion/WPFStockControlSystemWPFVersion/MainWindow.xaml.cs
+++ WPFStockControlSystemWPFVersion/WPFStockControlSystemWPFVersion/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPFStockControlSystemWPFVersion
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
