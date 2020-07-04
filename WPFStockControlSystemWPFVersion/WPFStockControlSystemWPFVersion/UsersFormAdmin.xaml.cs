b86d6458597209e352ea110ed12f847e7d846ae9
awaist7860 <48568803+awaist7860@users.noreply.github.com>
2020-07-04 19:32:13 +0100

    The Users form for Admin, currently works
    The Users form for Admin, currently works, problem is, with no dataGridView object we can't actually figure out a way to get data viewed in that Window.

--- WPFStockControlSystemWPFVersion/WPFStockControlSystemWPFVersion/UsersFormAdmin.xaml.cs
+++ WPFStockControlSystemWPFVersion/WPFStockControlSystemWPFVersion/UsersFormAdmin.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace WPFStockControlSystemWPFVersion
 {
@@ -22,6 +25,104 @@ namespace WPFStockControlSystemWPFVersion
         public UsersFormAdmin()
         {
             InitializeComponent();
+        }
+
+        string AccessString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+        private void btnViewUsers_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(AccessString);
+
+            try
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM Users", con); //This works
+                con.Open();
+
+                //Method 1
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show("exception occured while creating table:" + f.Message + "\t" + f.GetType());
+                con.Close();
+            }
+        }
+
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(AccessString);
+
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string adminAccess = cmbAdmin.Text;
+
+            try
+            {
+                if (username == "" || password == "" || adminAccess == "")
+                {
+                    MessageBox.Show("Please enter a username and a password and select admin access");
+                }
+                else
+                {
+                    SqlCommand sda = new SqlCommand("INSERT INTO Users VALUES ('" + username + "','" + password + "','" + adminAccess + "')", con); //This works
+                    con.Open();
+
+                    sda.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("User " + username + " has been created");
+                    btnViewUsers_Click(sender, e);
+                    txtPassword.Clear();
+                    txtUsername.Clear();
+                    cmbAdmin.Text = "";
+                }
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show("exception occured while creating table:" + f.Message + "\t" + f.GetType());
+                con.Close();
+            }
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(AccessString);
+
+            string username = txtDelUserName.Text;
+
+
+            try
+            {
+                if (username == "" || username == null)
+                {
+                    MessageBox.Show("Please enter a valid username");
+                }
+                else
+                {
+                    SqlCommand sda = new SqlCommand("DELETE FROM Users WHERE Username = '" + username + "'", con); //This works
+                    con.Open();
+
+                    sda.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("User " + username + " has been deleted");
+                    btnViewUsers_Click(sender, e);
+                    txtDelUserName.Clear();
+                }
+
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show("exception occured while creating table:" + f.Message + "\t" + f.GetType());
+                con.Close();
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Admin_Menu adminMenu = new Admin_Menu();
+            adminMenu.Show();
+            this.Close();
         }
     }
 }
